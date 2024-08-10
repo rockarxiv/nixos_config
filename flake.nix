3a96a406bc88d8b96801efc467e8c656aa7b186d
rockarxiv <rockarxiv@gmail.com>
2024-08-10 16:34:06 +0400

    init

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "A simple NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    envycontrol.url = "github:bayasdev/envycontrol";
+  };
+
+  outputs = { self, nixpkgs, envycontrol,... }@inputs: {
+    # Please replace my-nixos with your hostname
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        # Import the previous configuration.nix we used,
+        # so the old configuration file still takes effect
+        { _module.args = inputs; }
+        ./configuration.nix
+      ];
+    };
+  };
+}
